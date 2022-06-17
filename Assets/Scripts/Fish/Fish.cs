6aeaf39e76c5c38afd6214c2cbd63584be7b079b
ozantekce <32203867+ozantekce@users.noreply.github.com>
2022-06-17 20:16:18 +0300

    Bug fix -2

--- Assets/Scripts/Fish/Fish.cs
+++ Assets/Scripts/Fish/Fish.cs
@@ -40,39 +40,29 @@ public class Fish : MonoBehaviour
 
     public void ResetFish()
     {
-
-        if(tweener != null)
-        {
+        if (tweener != null)
             tweener.Kill(false);
-        }
 
-
-        float num = UnityEngine.Random.Range(type.minLenght,type.maxLenght);
-
+        float num = UnityEngine.Random.Range(type.minLenght, type.maxLenght);
         coll.enabled = true;
+
         Vector3 position = transform.position;
         position.y = num;
         position.x = screenLeft;
         transform.position = position;
 
         float num2 = 1;
-
-        float y = UnityEngine.Random.Range((num - num2), (num+num2));
-        Vector2 vector = new Vector2(-position.x, y);
+        float y = UnityEngine.Random.Range(num - num2, num + num2);
+        Vector2 v = new Vector2(-position.x, y);
 
         float num3 = 3;
-        float delay = UnityEngine.Random.Range(0, 2*num3);
-
-        tweener = transform.DOMove(vector, num3, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear).SetDelay(delay).OnStepComplete(delegate
+        float delay = UnityEngine.Random.Range(0, 2 * num3);
+        tweener = transform.DOMove(v, num3, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear).SetDelay(delay).OnStepComplete(delegate
         {
             Vector3 localScale = transform.localScale;
-            localScale.x *= -1;
+            localScale.x = -localScale.x;
             transform.localScale = localScale;
-
         });
-
-
-        
 
     }
 
